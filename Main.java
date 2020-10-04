6d778c28eb2d15b8f081e76e3637d1a4e6b418f6
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 14:30:39 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Count down by 5 until the user's integer
+ * @author Eric Seo
  */
 public class Main {
 
@@ -9,7 +10,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // Asks the user to enter a integer
+    System.out.println("Please enter an integer to count down to");
     
+    // Gets the number from the user
+    int numDown = input.nextInt();
+    
+    // declares a vaule for the variable count
+    int count = 100;
+
+    // Calculates the outcomes
+    while(count >= numDown){
+      System.out.print(count);
+      count = count - 5;
+    }
+    // I dont get why Im getting the out comes all in one line
   }
 }
